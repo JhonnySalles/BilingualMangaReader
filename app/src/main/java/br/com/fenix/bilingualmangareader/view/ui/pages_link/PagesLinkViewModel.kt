16bf900613cc6d455f581e882715e21aecfc327a
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2022-04-22 21:45:09 -0300

    Criação do model do vinculo das paginas.

--- app/src/main/java/br/com/fenix/bilingualmangareader/view/ui/pages_link/PagesLinkViewModel.kt
+++ app/src/main/java/br/com/fenix/bilingualmangareader/view/ui/pages_link/PagesLinkViewModel.kt
@@ -5,107 +5,35 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import br.com.fenix.bilingualmangareader.model.entity.Cover
+import br.com.fenix.bilingualmangareader.model.entity.FileLink
 import br.com.fenix.bilingualmangareader.model.entity.Manga
+import br.com.fenix.bilingualmangareader.model.entity.PageLink
 import br.com.fenix.bilingualmangareader.service.controller.ImageCoverController
 import br.com.fenix.bilingualmangareader.service.repository.CoverRepository
+import br.com.fenix.bilingualmangareader.service.repository.FileLinkRepository
 import br.com.fenix.bilingualmangareader.service.repository.MangaRepository
 
 class PagesLinkViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mContext = application.applicationContext
-    private val mMangaRepository: MangaRepository = MangaRepository(mContext)
+    private val mFileLinkRepository: FileLinkRepository = FileLinkRepository(mContext)
 
-    private var mListMangas = MutableLiveData<ArrayList<Manga>>(ArrayList())
-    val save: LiveData<ArrayList<Manga>> = mListMangas
+    private var mFileLink = MutableLiveData<FileLink>()
+    val fileLink: LiveData<FileLink> = mFileLink
+    private var mPagesLink = MutableLiveData<ArrayList<PageLink>>(ArrayList())
+    val pagesLink: LiveData<ArrayList<PageLink>> = mPagesLink
 
-    fun save(obj: Manga): Manga {
+    fun save(obj: FileLink): FileLink {
         if (obj.id == 0L)
-            obj.id = mMangaRepository.save(obj)
+            obj.id = mFileLinkRepository.save(obj)
         else
-            mMangaRepository.update(obj)
+            mFileLinkRepository.update(obj)
 
         return obj
     }
 
-    fun delete(obj: Manga) {
-        mMangaRepository.delete(obj)
-        remove(obj)
+    fun delete(obj: FileLink) {
+        mFileLinkRepository.delete(obj)
     }
 
-    fun get(position: Int): Manga? {
-        return if (mListMangas.value != null) mListMangas.value!!.removeAt(position) else  null
-    }
-
-    fun remove(manga: Manga) {
-        if (mListMangas.value != null)
-            mListMangas.value!!.remove(manga)
-    }
-
-    fun remove(position: Int) {
-        if (mListMangas.value != null)
-            mListMangas.value!!.removeAt(position)
-    }
-
-    fun update() {
-        if (mListMangas.value != null) {
-            for (manga in mListMangas.value!!) {
-                val item = mMangaRepository.get(manga.id!!)
-                manga.bookMark = item?.bookMark!!
-                manga.favorite = item.favorite
-                manga.lastAccess = item.lastAccess
-            }
-        }
-    }
-
-    fun update(list: List<Manga>) {
-        if (list.isNotEmpty()) {
-            for (manga in list) {
-                if (!mListMangas.value!!.contains(manga))
-                    mListMangas.value!!.add(manga)
-            }
-        }
-    }
-
-    private fun updateCover() {
-        if (mListMangas.value == null || mListMangas.value!!.isEmpty())
-            return
-
-        for (manga in mListMangas.value!!)
-            if (manga.thumbnail == null || manga.thumbnail!!.image == null)
-                ImageCoverController.instance.setImageCoverAsync(mContext, manga)
-    }
-
-
-    fun updateList() {
-        val list = mMangaRepository.list() ?: return
-        if (list.isNotEmpty()) {
-            for (manga in list) {
-                if (!mListMangas.value!!.contains(manga))
-                    mListMangas.value!!.add(manga)
-            }
-        }
-        updateCover()
-    }
-
-    fun list() {
-        val list = mMangaRepository.list()
-        if (list != null)
-            mListMangas.value = ArrayList(list)
-        else
-            mListMangas.value = ArrayList()
-    }
-
-    fun list(refreshComplete: () -> (Unit)) {
-        val list = mMangaRepository.list()
-        if (list != null) {
-            if (mListMangas.value == null || mListMangas.value!!.isEmpty())
-                mListMangas.value = ArrayList(list)
-            else
-                update(list)
-        } else
-            mListMangas.value = ArrayList()
-        refreshComplete()
-    }
-
-    fun isEmpty() : Boolean = mListMangas.value == null || mListMangas.value!!.isEmpty()
 }
