ef594bf3f49fad6217b1e7c2ece3e421a2344893
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2022-07-03 19:46:33 -0300

    Correção da data.

--- app/src/main/java/br/com/fenix/bilingualmangareader/view/ui/manga_detail/MangaDetailViewModel.kt
+++ app/src/main/java/br/com/fenix/bilingualmangareader/view/ui/manga_detail/MangaDetailViewModel.kt
@@ -15,7 +15,7 @@ import br.com.fenix.bilingualmangareader.util.constants.GeneralConsts
 import br.com.fenix.bilingualmangareader.util.helpers.Util
 import com.kttdevelopment.mal4j.manga.MangaPreview
 import java.io.File
-import java.time.LocalDateTime
+import java.util.*
 
 class MangaDetailViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -109,7 +109,7 @@ class MangaDetailViewModel(application: Application) : AndroidViewModel(applicat
 
     fun save(manga: Manga?) {
         manga ?: return
-        manga.lastAccess = LocalDateTime.now()
+        manga.lastAccess = Date()
 
         if (manga.id == 0L)
             manga.id = mMangaRepository.save(manga)
