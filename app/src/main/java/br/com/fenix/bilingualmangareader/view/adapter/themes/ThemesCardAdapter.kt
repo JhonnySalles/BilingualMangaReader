dae98fb56fed9c4432f10805d2f3ac5c6d0fa00d
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2022-09-09 22:28:00 -0300

    Ajuste para transformar o thema em lista.

--- app/src/main/java/br/com/fenix/bilingualmangareader/view/adapter/themes/ThemesCardAdapter.kt
+++ app/src/main/java/br/com/fenix/bilingualmangareader/view/adapter/themes/ThemesCardAdapter.kt
@@ -1,36 +1,65 @@
 package br.com.fenix.bilingualmangareader.view.adapter.themes
 
+import android.content.Context
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import android.widget.BaseAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
 import br.com.fenix.bilingualmangareader.R
 import br.com.fenix.bilingualmangareader.model.enums.Themes
 import br.com.fenix.bilingualmangareader.service.listener.ThemesListener
+import br.com.fenix.bilingualmangareader.util.helpers.Util
+import com.google.android.material.card.MaterialCardView
 
-class ThemesCardAdapter : RecyclerView.Adapter<ThemesViewHolder>() {
+class ThemesCardAdapter(var context: Context, listener: ThemesListener) : BaseAdapter() {
 
-    private lateinit var mListener: ThemesListener
+    companion object {
+        var mPageSelectStroke: Int = 0
+    }
+
+    init {
+        mPageSelectStroke = context.resources.getDimension(R.dimen.config_theme_selected_stroke).toInt()
+    }
+
+    private var mListener: ThemesListener = listener
     private var mList: MutableList<Pair<Themes, Boolean>> = mutableListOf()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ThemesViewHolder {
-        return ThemesViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.grid_card_theme, parent, false), mListener)
-    }
-
-    override fun onBindViewHolder(holder: ThemesViewHolder, position: Int) {
-        holder.bind(mList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return mList.size
-    }
-
-    fun attachListener(listener: ThemesListener) {
-        mListener = listener
-    }
 
     fun updateList(list: MutableList<Pair<Themes, Boolean>>) {
         mList = list
         notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        return mList.size
+    }
+
+    override fun getItem(index: Int): Pair<Themes, Boolean> {
+        return mList[index]
+    }
+
+    override fun getItemId(index: Int): Long {
+        return index.toLong()
+    }
+
+    override fun getView(index: Int, view: View?, parent: ViewGroup?): View {
+        val theme = getItem(index)
+
+        val convertView = LayoutInflater.from(context)
+                .cloneInContext(ContextThemeWrapper(context, mList[index].first.getValue()))
+                .inflate(R.layout.grid_card_theme, parent, false)
+
+        convertView?.findViewById<MaterialCardView>(R.id.theme_card)?.strokeWidth = if (theme.second) mPageSelectStroke else 0
+        convertView?.findViewById<TextView>(R.id.theme_name)?.text = Util.themeDescription(context, theme.first)
+
+        convertView?.findViewById<LinearLayout>(R.id.theme_root)?.setOnClickListener {
+            mListener.onClick(theme)
+            true
+        }
+
+        return convertView
     }
 
 }
